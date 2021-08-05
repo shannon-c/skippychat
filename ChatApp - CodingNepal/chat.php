e71973eb3fbeb30a9142b2271721deabfff4a800
Shannon Chan <73507495+shannon-c@users.noreply.github.com>
2021-08-05 16:33:39 +0800

    Add files via upload

---
+++ ChatApp - CodingNepal/chat.php
@@ -0,0 +1,54 @@
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src = "js/emojionearea.min.js"></script>
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="chat-area">
+      <header>
+        <?php 
+          $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }else{
+            header("location: users.php");
+          }
+        ?>
+        <a href="users.php" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+        <img src="php/images/<?php echo $row['img']; ?>" alt="">
+        <div class="details">
+          <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+          <p><?php echo $row['status']; ?></p>
+        </div>
+      </header>
+      <div class="chat-box">
+
+      </div>
+       
+
+
+      <form action="#" class="typing-area">
+        <input type="text" class="incoming_id" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+        <input type="text" name="message" class="input-field" placeholder="Type a message here..." autocomplete="off">
+          <button><i class="fab fa-telegram-plane"></i></button>
+      </form>
+    </section>
+  </div>
+<script>
+    $(document).ready(function(){
+        $("#")
+    })
+</script>
+
+  <script src="javascript/chat.js"></script>
+
+</body>
+</html>
